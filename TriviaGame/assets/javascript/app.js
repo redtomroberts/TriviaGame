76d2564b969cd8502dfa813afb7dacee501d8a8e
redtomroberts <55162586+redtomroberts@users.noreply.github.com>
2019-10-20 23:59:23 -0400

    Add files via upload

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,23 @@
+/* Variables ==================================================================*/
+const quiz = document.getElementById('quizContainer');
+const results = document.getElementById('resultContainer');
+const submit = document.getElementById('subBtnContainer');
+
+var time = 20;
+/* Functions==================================================================*/
+function startGame()
+{
+    
+}
+
+function gamePlay()
+{
+
+$('.startBtn').on('click', function()
+    {
+    $('button').remove('.startBtn')
+});
+
+function timeCount()
+    {
+        setInterval(funtion()
